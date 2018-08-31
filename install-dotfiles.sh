0c11487a154b14cc5109a8364a07f58ef715712b
Tom Jennings <tomwjennings@gmail.com>
2018-08-31 13:23:23 +0100

    Update installer for Ubuntu

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+# Install packages
+sudo apt-get install neovim curl
+
+# Configure nvim
+mv ~/.config/nvim ~/.config/nvim.backup
 ln -s ~/dotfiles/nvim ~/.config/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+
+# Configure git
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
