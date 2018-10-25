f923dc55c44c05eaff23b3d95aa0893e9ff8316d
Tom Jennings <tomwjennings@gmail.com>
2018-10-25 17:02:43 +0100

    Added windows config

--- bashrc
+++ bashrc
@@ -4,4 +4,14 @@ for config in "$HOME"/dotfiles/bashrc.d/*.bash ; do
 done
 unset -v config
 
+# If we're running in Windows setup docker
+if [[ $(uname -v) == *"Microsoft"* ]]; then 
+  export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+  export PATH="$PATH:/mnt/c/Program\ Files/Docker/Docker/resources/bin"
+  alias docker=docker.exe
+  alias docker-compose=docker-compose.exe
+fi
+
 export PATH=$PATH:~/dotfiles/bin
+source ~/src/github.com/jdxcode/gh/bash/gh.bash
+source ~/src/github.com/jdxcode/gh/completions/gh.bash
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,12 +1,44 @@
-#!/bin/sh
+#!/bin/bash -x
 
+function setupRCFile {
+  rcfile=~/.$1
+  # Configure git
+  if [[ -f $rcfile ]]; then
+    mv $rcfile $rcfile.bkp
+  fi
+  ln -s ~/dotfiles/$1 $rcfile
+}
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+sudo apt-get install -y curl 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
 # Install packages
-sudo apt-get install neovim curl
+sudo apt-get install -y neovim \
+	             curl \
+		     ansible \
+		     npm \
+                     silversearcher-ag \
+		     thefuck \
+		     kubectl \
+		     nodejs \
+		     git \
+		     shellcheck
 
+# Setup gh
+mkdir -p ~/src/github.com/jdxcode/gh
+cd ~/src/github.com/jdxcode
+git clone git@github.com:jdxcode/gh.git
+
+python-pkg-resources mv ~/.config/nvim ~/.config/nvim.backup 
 # Configure nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
+ln -s nvim ~/.config/nvim
+setupRCFile vimrc
+setupRCFile gitconfig
+setupRCFile bashrc
 
-# Configure git
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
